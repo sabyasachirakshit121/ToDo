6ee7157fafd20a44857ba569e0cb32f831885799
sabyasachirakshit <sabyasachirakshitacs19@gmail.com>
2022-12-09 12:09:23 +0530

    first commit

---
+++ main/views.py
@@ -0,0 +1,23 @@
+from rest_framework import generics
+from .serializers import *
+from .models import *
+
+
+class ListTodo(generics.ListAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = ToDoSerializer
+
+
+class DetailTodo(generics.RetrieveUpdateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = ToDoSerializer
+
+
+class CreateTodo(generics.CreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = ToDoSerializer
+
+
+class DeleteTodo(generics.DestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = ToDoSerializer
